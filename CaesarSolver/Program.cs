9f624b03230ba266e944d0c81ee824109136c5dd
the f <feldrim@gmail.com>
2018-11-04 00:17:46 +0300

    Added CaesarSolver class with 3 ctors and Solve method

--- CaesarSolver/Program.cs
+++ CaesarSolver/Program.cs
@@ -2,11 +2,19 @@
 
 namespace CaesarSolver
 {
-   class Program
+   internal class Program
    {
-      static void Main(string[] args)
+      public static void Main()
       {
-         Console.WriteLine("Hello World!");
+         const string encryptedText = "Ypw ewk onDEuAu rvLMyM VCS ZQX FXL 13TXY X3 b0Y eXb21fp. db wnu vkmsq GxE mEs HvswS BLP TIG TFX06, SQ4S Y9 h8fd 3a07 XpVXa5g3qt4h8";
+         const string charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+         const string ignoredChars = " .,";
+
+         var cs = new CaesarSolver(charset, ignoredChars);
+         var res = cs.Solve(encryptedText);
+
+         Console.WriteLine(res);
+         Console.ReadLine();
       }
    }
 }
